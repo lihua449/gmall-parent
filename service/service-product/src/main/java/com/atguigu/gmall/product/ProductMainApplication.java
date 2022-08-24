a62a9925a198de08b46c56e170819247a3791237
李耀华 <2825260985@qq.com>
2022-08-25 01:39:58 +0800

    day02进度

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/ProductMainApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.gmall.product;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @author: 李耀华
+ * @date: 2022/8/24 22:47
+ */
+
+@SpringCloudApplication
+@MapperScan("com.atguigu.gmall.product.mapper")
+public class ProductMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMainApplication.class,args);
+    }
+}
